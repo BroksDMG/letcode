5d800bce5631c6959886fbce4264eea1702b7aa1
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-15 15:43:56 +0100

    Defanging an IP Address

---
+++ Defanging_an_IP_Address/solution.js
@@ -0,0 +1,9 @@
+'use strict'
+/**
+ * @param {string} address
+ * @return {string}
+ */
+ var defangIPaddr = function(address) {
+    return address.replaceAll('.',"[.]")
+};
+console.log(defangIPaddr("255.100.50.0"));
