a181b398f049ed93b6facab49686e9311ca456c0
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-18 23:24:20 +0100

    Number_of_pair

--- Richest_Customer_Wealth/solution.js
+++ Richest_Customer_Wealth/solution.js
@@ -6,17 +6,27 @@
     let count=0;
     let res=0;
     for(let i=0;i<accounts.length;i++){
-        // console.log("Acc[i] :"+accounts[i])
-        // console.log(accounts[i].length);
-        for(let j=0;j<accounts.length;j++){
-        //   console.log(accounts[j].length);
-          count+= accounts[j]
-          res=Math.max(res);
-          console.log("Acc[j] :"+res);
-          
-        //   console.log("count :"+count);
-        }
+        console.log("Acc[iiiii] :"+accounts[i])
+        // console.log(accounts.length);
+        
         
     }
+    // for(let i=0;i<accounts.length;i++){
+    //     console.log("Acc[iiiii] :"+accounts[i])
+    //     // console.log(accounts.length);
+    //     console.log(count+=accounts[i])
+    //     for(let j=0;j<accounts[i].length;j++){
+    //     //   console.log(accounts[j].length);
+    //       console.log("Acc[j] :"+accounts[j]);
+    //     }
+        
+    // }
 };
-maximumWealth([[1,2,3],[3,2,1],[3,2,1],[3,2,1],[3,2,1],[3,2,1]])
+maximumWealth(  [1,2,3])
+// maximumWealth(
+//     [   [1,2,3],
+//         [3,2,1],
+//         [4,3,1],
+//         [6,1,1],
+//         [9,2,2],
+//         [5,7,2]])
