d701c8806d5ddd51bf106d137d193d2f4728690e
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-04-14 12:47:38 +0200

    Counter

--- Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits/solution.js
+++ Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits/solution.js
@@ -1,20 +1,26 @@
+
 /**
  * @param {number} num
  * @return {number}
  */
  var minimumSum = function(num) {
     const arr =Array.from(String(num),Number)
-    
+    const arr1 =[arr.toLocaleString()]
+    console.log(arr1);
     for(let i=1;i<arr.length;i++){
         const raz=[...arr.slice(0,i)];
         const dwa=[...arr.slice(raz.length,4)];
+        // arr.push(arr[arr.length])
+        // arr.shift()
+        console.log(arr1.length)
+        console.log(arr1);
         
-        console.log(arr.replace(2,3));
         // console.log(map);
         // const dwa =last.slice(last.length,).join('')
         console.log(raz);
         console.log(dwa)
         // console.log(last[i]);
     }
+    
 };
 minimumSum(2932)
