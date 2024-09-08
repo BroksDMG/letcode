67f6c3dc6ca2f21183e3163fcb5e9447b37217ba
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-09-08 17:23:21 +0200

    Prefix task done

--- Longest_Common_Prefix/solution.js
+++ Longest_Common_Prefix/solution.js
@@ -3,48 +3,20 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  //input: array
-  //znaleźć podobne pefixy czyli litery tak takie same
-  //każdy element tablicy musi być sprawdzony
-  //później zbierać elementy i porównywać czy są takie same
-
-  let res = "";
-  let prefix = "";
-  let result = "";
-  let first;
-  let second;
-  for (let i = 0; i < strs.length; i++) {
-    // "flower", "flow", "flight"] => "fl"
-    // ""flower", "tlight", "flow"]"=> "flow" czy ""
-
-    for (let j = 0; j < strs[i].length; j++) {
-      //'f' 'l' 'o' 'w' itd..
-      first = strs[i][j];
-      second = strs[i + 1][j];
-      if (first === second) prefix += second;
-      else {
-        continue;
-      }
-      //   console.log("first", first, "second", second, "third", prefix);
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    // console.log("prefix", prefix, "str");
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
     }
-    prefix += " ";
-    if (prefix.length > result.length) console.log(prefix.split(" "));
   }
-
-  //   console.log("asd");
-  //output:string
+  return prefix;
 };
 
-console.log(
-  longestCommonPrefix([
-    "flower",
-    "flow",
-    "flight",
-    "flower",
-    "flow",
-    "flight",
-    "flower",
-    "flow",
-    "flight",
-  ])
-);
+// longestCommonPrefix(["flower", "flow"]);
+console.log(longestCommonPrefix(["flower", "flow", "flaght"]));
+console.log(longestCommonPrefix(["flower", "flower", "flower"]));
+console.log(longestCommonPrefix([""]));
+console.log(longestCommonPrefix(["a"]));
