4cb25e6e934951a391ea294772ea60adad555623
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-09-20 21:56:15 +0200

    product of array done but i need more time to understand solution

--- Product_o_Arra_Except_Self/solution.js
+++ Product_o_Arra_Except_Self/solution.js
@@ -1,46 +1,29 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number[]}
-//  */
-// var productExceptSelf = function (nums) {
-//   let answerarr = [];
-//   for (let i = 0; i < nums.length; i++) {
-//     let curmul = 1;
-//     for (let j = 0; j < nums.length; j++) {
-//       if (i === j) continue;
-//       curmul *= nums[j];
-//     }
-//     answerarr.push(curmul);
-//   }
-//   return answerarr;
-// };
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  let answerarr = new Array(nums.length).fill(1);
+  let left = 1;
+  for (let i = 1; i < nums.length; i++) {
+    answerarr[i] = left;
+    left *= nums[i];
+    // console.log(answerarr);
+  }
+  let right = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answerarr[i] *= right;
+    right *= nums[i];
+    console.log(answerarr);
+  }
+  return answerarr;
+};
 //pomnożyć ze sobą każdą liczbe oprócz a[i] i dodać do tablicy
 console.log(productExceptSelf([1, 2, 3, 4]));
-console.log(productExceptSelf([-1, 1, 0, -3, 3]));
-console.log(productExceptSelf([-1]));
+// console.log(productExceptSelf([-1, 1, 0, -3, 3]));
+// console.log(productExceptSelf([-1]));
 // Input: nums = [1,2,3,4]
 // Output: [24,12,8,6]
 
 // Input: nums = [-1,1,0,-3,3]
 // // Output: [0,0,9,0,0]
-
-var productExceptSelf = function (nums) {
-  const n = nums.length;
-  const answer = new Array(n).fill(1);
-
-  // Oblicz iloczyny z lewej strony dla każdego elementu
-  let leftProduct = 1;
-  for (let i = 0; i < n; i++) {
-    answer[i] = leftProduct;
-    leftProduct *= nums[i];
-  }
-
-  // Oblicz iloczyny z prawej strony i pomnóż je przez obliczone wcześniej wartości
-  let rightProduct = 1;
-  for (let i = n - 1; i >= 0; i--) {
-    answer[i] *= rightProduct;
-    rightProduct *= nums[i];
-  }
-
-  return answer;
-};
