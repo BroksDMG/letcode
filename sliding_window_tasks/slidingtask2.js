de39cfdc812b8b2b309070f3f4ae6d503324e689
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-10-07 21:45:13 +0200

    next task done

--- sliding_window_tasks/slidingtask2.js
+++ sliding_window_tasks/slidingtask2.js
@@ -1,24 +1,31 @@
 function sliding(arr, k) {
-  let sumwindow = 0;
-  let max = 0;
-  let countRow = 0;
-  let secendZero = 0;
-  for (let i = 0; i < k; i++) {
-    countRow++;
-    sumwindow += arr[i];
-    if (arr[i] === 0) secendZero++;
-    if (secendZero >= 2) {
-      countRow = 0;
-      secendZero = 0;
+  let maxOnes = 0; // Maksymalna liczba jedynek po zamianie zer na jedynki
+  let windowOnesCount = 0; // Liczba jedynek w obecnym oknie
+  let left = 0; // Lewy wskaźnik okna
+
+  // Przesuwamy prawy wskaźnik po tablicy
+  for (let right = 0; right < arr.length; right++) {
+    // Zwiększamy licznik jedynek, jeśli element na pozycji 'right' to 1
+    if (arr[right] === 1) {
+      windowOnesCount++;
     }
-    console.log(sumwindow, countRow, secendZero);
+
+    // Jeśli okno ma długość większą niż 'k', przesuwamy lewy wskaźnik
+    if (right - left + 1 > k) {
+      if (arr[left] === 1) {
+        windowOnesCount--;
+      }
+      left++;
+    }
+
+    // Liczba jedynek = bieżąca liczba jedynek w oknie + liczba zer, które możemy zamienić
+    maxOnes = Math.max(
+      maxOnes,
+      windowOnesCount + (k - (right - left + 1 - windowOnesCount))
+    );
   }
-  max = sumwindow;
-  for (let i = k; i < arr.length; i++) {
-    sumwindow = sumwindow - arr[i - k] + arr[i];
-    // console.log(sumwindow);
-  }
-  //   console.log(max);
+
+  return maxOnes;
 }
 console.log(sliding([1, 1, 0, 0, 1, 0, 1, 0, 1, 1], 5));
 // console.log(sliding([0, 0, 1, 1, 1, 0, 1], 4));
